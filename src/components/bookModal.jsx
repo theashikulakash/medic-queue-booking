0bba4773c508dd85c3d63a5fefc35f225d4670b5
Md Ashikul Islam <akashashikul@gmail.com>
2026-05-20 19:22:12 +0600

    deployment check

--- src/components/bookModal.jsx
+++ src/components/bookModal.jsx
@@ -76,7 +76,7 @@ const BookModal = ({ defaultDoctorId, defaultDoctorName }) => {
                             </Modal.Icon>
                             <Modal.Heading>Fill this form to book appointment</Modal.Heading>
                             <p className="mt-1.5 text-sm leading-5 text-muted">
-                                Fill out the form below and we'll get back to you. The modal adapts automatically
+                                Fill out the form below and we&apos;ll get back to you. The modal adapts automatically
                                 when the keyboard appears on mobile.
                             </p>
                         </Modal.Header>
--- src/components/doctorCard.jsx
+++ src/components/doctorCard.jsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { authClient } from '@/lib/auth-client';
 import BookModal from '@/components/bookModal';
 import { Button } from '@heroui/react';
+import Image from 'next/image';
 
 const DoctorCard = ({ doctor }) => {
     const { _id, id, name, specialty, image, experience, availability, description, hospital, location, fee } = doctor;
@@ -16,9 +17,11 @@ const DoctorCard = ({ doctor }) => {
         <section>
             <article className="group  overflow-hidden rounded-3xl border border-slate-200 bg-white shadow-sm transition duration-200 hover:-translate-y-1 hover:shadow-lg">
                 <div className="relative">
-                    <img
+                    <Image
                         src={image}
                         alt={name}
+                        width={720}
+                        height={224}
                         className="h-56 w-full object-cover"
                     />
                     <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-black/80 to-transparent px-4 py-3 text-white">
@@ -61,7 +64,7 @@ const DoctorCard = ({ doctor }) => {
                                 variant="danger"
                                 className="w-full rounded-2xl px-4 py-3 text-sm font-semibold transition"
                             >
-                                Doctor's Details
+                                Doctor&apos;s Details
                             </Button>
                         </Link>
 
